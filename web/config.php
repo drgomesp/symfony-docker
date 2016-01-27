11316d8efe96a225d6901f20064e3091809f753c
drgomesp <daniel.ribeiro@propertyfinder.ae>
2016-01-27 15:19:08 +0400

    Added and updated dependencies

--- web/config.php
+++ web/config.php
@@ -16,7 +16,6 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
-    '192.168.99.1',
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
